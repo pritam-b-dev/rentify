c7aa4be7ea4a219ee0dff24539d68dd0825a5c11
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-05-21 15:04:33 +0600

    added jwt through betterauth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("rentify");
@@ -15,7 +16,9 @@ export const auth = betterAuth({
   trustedOrigins: ["http://localhost:3000"],
   session: {
     cookieCache: {
-      enabled: false,
+      enabled: true,
+      strategy: "jwt",
+      maxAge: 7 * 24 * 60 * 60,
     },
   },
   socialProviders: {
@@ -24,4 +27,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
   },
+  plugins: [jwt()],
 });
